93c9ceb30488f5e5d9c7b571d4a266ed881f632f
Pedro Carara <cararax@gmail.com>
2021-02-10 12:16:32 -0300

    adicionando anotação @springbootapplicaiton e metodo main na classe principal

--- src/main/java/br/com/ufsm/pedidoService/pedidoservice/ProdutoApplication.java
+++ src/main/java/br/com/ufsm/pedidoService/pedidoservice/ProdutoApplication.java
@@ -1,10 +1,13 @@
 package br.com.ufsm.pedidoService.pedidoservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ProdutoApplication {
 
 	public static void main(String[] args) {
-		
-
+		SpringApplication.run(ProdutoApplication.class, args);
 	}
 
 }
