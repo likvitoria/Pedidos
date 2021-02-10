48dff81a38388dc82f92ef76b88cf94a7c6162bb
Pedro Carara <cararax@gmail.com>
2021-02-10 12:38:58 -0300

    resolvendo erro field require bean of type that could not be found

--- src/main/java/br/com/ufsm/pedidoService/pedidoservice/ProdutoApplication.java
+++ src/main/java/br/com/ufsm/pedidoService/pedidoservice/ProdutoApplication.java
@@ -3,7 +3,8 @@ package br.com.ufsm.pedidoService.pedidoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={
+		"br.com.ufsm.pedidoService.pedidoservice.repository"})
 public class ProdutoApplication {
 
 	public static void main(String[] args) {
